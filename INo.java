44abd588152b5c7a8210a4bc06da30eedb9089ff
gustavomontanini <168143050+gustavomontanini@users.noreply.github.com>
2025-04-13 18:47:55 -0300

    Delete INo.java

--- INo.java
+++
@@ -1,8 +0,0 @@
-public interface INo {
-    No getProx();
-    void setProx(No prox);
-    Object getConteudo();
-    void setConteudo(Object conteudo);
-    No getAnterior();
-    void setAnterior(No anterior);
-}
