29106036bece8e85e562b389b54b65cf98687971
gustavomontanini <168143050+gustavomontanini@users.noreply.github.com>
2025-04-13 18:47:03 -0300

    Add files via upload

---
+++ Atividade_Final-AED/src/INo.java
@@ -0,0 +1,8 @@
+public interface INo {
+    No getProx();
+    void setProx(No prox);
+    Object getConteudo();
+    void setConteudo(Object conteudo);
+    No getAnterior();
+    void setAnterior(No anterior);
+}
